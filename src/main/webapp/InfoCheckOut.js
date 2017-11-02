19941d4f274e886caecbb1bd78af52e92c233a79
Juan Manuel <Juan Manuel@172.25.17.15>
2017-11-02 14:48:56 -0500

    Ubicación usuario

--- src/main/webapp/InfoCheckOut.js
+++ src/main/webapp/InfoCheckOut.js
@@ -40,3 +40,43 @@ $(document).ready(function(){
         });
         }
         });
+
+
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 4.714666, lng: -74.072115},
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+          zoom: 15
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+        
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Tú Ubicación <3');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: El servicio de geolocalización falló.' :
+                              'Error: Tu explorador no soporta la geolocalización.');
+      }
