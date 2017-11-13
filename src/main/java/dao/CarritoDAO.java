bfc49aa2b6ffab919418133ed012330638539101
santirogo <santirogo@gmail.com>
2017-11-12 19:22:55 -0500

    Update CarritoDAO.java

--- src/main/java/dao/CarritoDAO.java
+++ src/main/java/dao/CarritoDAO.java
@@ -74,6 +74,7 @@ public class CarritoDAO {
                         prod.setNombre(rs.getString(2));
                         prod.setCategoria(rs.getString(3));
                         prod.setPrecio(rs.getInt(4));
+                        prod.setRutaImagen(rs.getString(5));
                         prod.setCantidad((int) producto.get(2));
                         prod.setTienda(rs.getInt(7));
                         System.out.println("AIDI:" + rs.getInt(7));
